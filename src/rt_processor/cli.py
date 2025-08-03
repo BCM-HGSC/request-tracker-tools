647ced650f8d1a6df71473b3564006b4ac12d5d6
Walker Hale IV <walker.hale.iv@gmail.com>
2025-08-03 15:50:57 -0500

    Rename argument parser

--- src/rt_processor/cli.py
+++ src/rt_processor/cli.py
@@ -7,14 +7,14 @@ from .session import RTSession
 
 def main():
     """Main entry point for the RT processor CLI."""
-    args = parse_arguments()
+    args = parse_main_arguments()
     with RTSession() as session:
         session.authenticate()
         session.print_cookies()
         session.try_url(args.id_string, *args.parts)
 
 
-def parse_arguments() -> Namespace:
+def parse_main_arguments() -> Namespace:
     """Parse command line arguments."""
     parser = ArgumentParser(description="Communicate with RT")
     parser.add_argument("id_string", help="ID string to append to base URL")
